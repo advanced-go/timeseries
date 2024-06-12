ed1902f800701dea2150ab7cf1b2bd76b415c8dc
advanced-go <mark.wilson.go@outlook.com>
2024-06-12 08:48:28 -0500

    Create package.go

---
+++ assignment/package.go
@@ -0,0 +1,6 @@
+package assignment
+
+const (
+	assignmentZone = "assignment/zone" // Should return unique host names for region/zone/sub-zone
+	assignmentHost = "assignment/host" // Should return unique instance id's region/zone/sub-zone/host
+)
